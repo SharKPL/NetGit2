9b4f3e92e3398fd046c714deac250adcdefc557d
EvilWolly <TimberWolfX@yandex.ru>
2025-06-20 21:25:10 +0300

    ww

--- Net/Assets/MyAssets/Scripts/Player/PlayerCamera.cs
+++ Net/Assets/MyAssets/Scripts/Player/PlayerCamera.cs
@@ -56,6 +56,7 @@ namespace MUSOAR
 
         private void LateUpdate()
         {
+            if (!isLocalPlayer) return;
             GetCameraInput();
             ApplyRotation();
             ApplyDeathRotation();
